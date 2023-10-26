ba8d7d67ee7eec26200685b116927265fdbfe281
Allan Almeida <allansoares.almeida@gmail.com>
2023-10-26 00:01:04 -0300

    Criando a tabela para exibição e pesquisa dos eventos

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+  public larguraImagem: number = 150;
+  public margemImagem: number = 2;
+  public mostrarImagem: boolean = true;
+  private _filtroLista: string = '';
+
 
   constructor(private http: HttpClient) { }
 
@@ -18,9 +25,32 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/evento').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
+  }
+
+  public get filtroLista(): string  {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+
+  exibirImagem(){
+    this.mostrarImagem = !this.mostrarImagem;
   }
 
 }
